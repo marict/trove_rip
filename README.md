120423d44d8d61298e165f3640cebcae2dab0fe4
Paul Curry <Paul Curry>
2020-05-03 21:52:02 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # trove_rip
-Python3.8 Script to extract images from The Trove directories
+Python3.8 Script to extract images from The Trove directories.
+
+Setup:
+Install python3.8
+then
+```
+pip install request
+pip install bs4
+```
+
+set the @site variable to the folder with the images you want.
---
+++ trove_rip.py
@@ -0,0 +1,49 @@
+import re
+import requests
+import os
+import tqdm
+import sys
+from bs4 import BeautifulSoup
+
+# site = 'http://pixabay.com'
+# site = 'https://thetrove.net/Assets/Map%20Assets/Sewers/Sewer_Pipes_id1/'
+
+
+def write_images(site):
+    print("Writing images for site: {}".format(site))
+    storage_dir = site.split('/')[-2]
+    if not os.path.exists(storage_dir):
+        os.mkdir(storage_dir)
+
+    response = requests.get(site)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    urls = [x.get('href') for x in soup.find_all('a')]
+
+    for url in tqdm.tqdm(urls,unit="link"):
+        if url == "./Bandit%201.png":
+            import pdb; pdb.set_trace()
+            
+        # filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+        filename = re.search(r'.+[.](jpg|gif|png)$', url)
+        if not filename:
+            print("Regex didn't match with the url: {}".format(url))
+            continue
+        with open(storage_dir + '/' + filename.group(0), 'wb') as f:
+            print("\tWriting: {}".format(filename.group(0)))
+            if 'http' not in url:
+                # sometimes an image source can be relative 
+                # if it is provide the base url which also happens 
+                # to be the site variable atm. 
+                url = '{}{}'.format(site, url)
+            response = requests.get(url)
+            f.write(response.content)
+
+args = sys.argv
+if args[1] is not None:
+    site = args[1]
+    import pdb; pdb.set_trace()
+else:
+    site = "https://thetrove.net/Assets/Map%20Assets/Terrain%20-%20cover/Rubble/"
+write_images(site)
+
+print("FINISHED")
